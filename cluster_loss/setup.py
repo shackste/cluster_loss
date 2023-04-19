df40dba94f9e8a2f5d46bc4ebfa829fb75c277ff
Shackste <shackst3@gmail.com>
2023-04-19 08:58:30 +0200

    Initial commit

---
+++ cluster_loss/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cluster_loss',
+    version='0.1',
+    description='Package for Pytorch Loss functions based on clusters in the target set with the purpose of fitting the target distribution',
+    author='Stefan Hackstein',
+    author_email='shackst3@gmail.com',
+    packages=['cluster_loss'],
+    install_requires=['kmeans_pytorch', 'torch', 'numpy', 'geomloss']
+)
